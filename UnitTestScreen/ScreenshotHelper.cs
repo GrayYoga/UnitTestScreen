3a0582fcfe8dcaf1c1607eda2476027edac6dd3d
Sergei.Boiko <sergei.boiko@simbirsoft.com>
2017-10-18 17:57:08 +0400

    Add ScreenshotHelper.cs

---
+++ UnitTestScreen/ScreenshotHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System.IO;
+
+namespace UnitTestScreen
+{
+    public class ScreenshotHelper
+    {
+        private RemoteWebDriver _remoteWebDriver;
+
+        public ScreenshotHelper(RemoteWebDriver remoteWebDriver)
+        {
+            _remoteWebDriver = remoteWebDriver;
+        }
+        private string TakeScreenshot(string screenshotDirectoryPath)
+        {
+            var screenshot = ((ITakesScreenshot)_remoteWebDriver).GetScreenshot();
+            var directoryPath = $"{screenshotDirectoryPath}\\images";
+            Directory.CreateDirectory(directoryPath);
+            var filePath = $"{directoryPath}\\{DateTime.Now.ToString("yyyyMMdd-HHmmss")}-{Guid.NewGuid()}.png";
+            screenshot.SaveAsFile(filePath, ScreenshotImageFormat.Png);
+            return filePath;
+        }
+        private string TakeB64Screenshot()
+        {
+            var screenshot = ((ITakesScreenshot)_remoteWebDriver).GetScreenshot();
+            return screenshot.AsBase64EncodedString;
+        }
+        public string TakeScreenshotAndGetLink(string screenshotDirectoryPath)
+        {
+            string filePath = TakeScreenshot(screenshotDirectoryPath);
+            return $"<a href = \"file://{filePath.Replace('\\', '/').Replace(':', '|')}\" target = \"_blank\"> " +
+                $"<img src = \"file://{filePath.Replace('\\', '/').Replace(':', '|')}\" alt = \"Screensot\" width = \"100%\" ></a><br>";
+        }
+        public string TakeScreenshotAndGetBase64Link()
+        {
+            string filePath = TakeB64Screenshot();
+            return $"<a href = \"data:image/png;base64,{TakeB64Screenshot()}\" target = \"_blank\"> " +
+                $"<img src = \"data:image/png;base64,{TakeB64Screenshot()}\" alt = \"Screensot\" width=\"100%\" ></a><br>";
+        }
+    }
+}
