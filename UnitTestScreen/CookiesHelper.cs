5f7f5bfb2115e03e36af8570be91c4458a88fbc4
Sergei.Boiko <sergei.boiko@simbirsoft.com>
2017-10-23 02:06:15 +0400

    Авторизация с автосохранением и повторным использованием cookies.

---
+++ UnitTestScreen/CookiesHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+
+namespace UnitTestScreen
+{
+    public class CookiesHelper
+    {
+        
+        IEnumerable<Cookie> _cookies;
+        public IEnumerable<Cookie> Cookies {
+            get { return _cookies; }
+            set
+            {
+                _cookies = value;
+                using (StreamWriter streamWriter = new StreamWriter(this.HostName))
+                {
+                    streamWriter.Write(JsonConvert.SerializeObject(_cookies,
+                        new JsonSerializerSettings{TypeNameHandling = TypeNameHandling.All}));
+                }
+            }
+        }
+
+        public bool IsSet { get; }
+        private string HostName;
+        public CookiesHelper(string HostName = null)
+        {
+            if(HostName != null)
+            {
+                // проверить наличие файла
+                this.HostName = HostName;
+                if (File.Exists(this.HostName))
+                {
+                    IsSet = true;
+                    using (StreamReader streamReader = new StreamReader(this.HostName))
+                    {
+                        _cookies = JsonConvert.DeserializeObject<IEnumerable<Cookie>>(streamReader.ReadToEnd(),
+                            new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+                    }
+                }
+            }
+        }
+    }
+}
--- UnitTestScreen/UnitTestWithScreenshot.cs
+++ UnitTestScreen/UnitTestWithScreenshot.cs
@@ -2,8 +2,12 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Chrome;
+
 using NUnit.Framework;
 using System.Threading;
+using System.IO;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
 
 namespace UnitTestScreen
 {
@@ -11,17 +15,47 @@ namespace UnitTestScreen
     public class UnitTestWithScreenshot
     {
         private RemoteWebDriver driver;
+
         [OneTimeSetUp]
         public void RunDriver()
         {
             driver = new RemoteWebDriver(new ChromeOptions().ToCapabilities());
-            driver.Navigate().GoToUrl("https://www.simbirsoft.com");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
+
         [Test]
         public void TestMethod()
         {
-            Assert.AreEqual(1, 2, new ScreenshotHelper(driver).TakeScreenshotAndGetBase64Link(), new object[] { });
+            const string resourceUri = @"http://courses.way2automation.com";
+            //Assert.AreEqual(1, 2, new ScreenshotHelper(driver).TakeScreenshotAndGetBase64Link(), new object[] { });
+            driver.Navigate().GoToUrl(resourceUri);
+            // insert cookies managing
+            CookiesHelper cookiesHelper = new CookiesHelper(
+                Path.Combine( TestContext.CurrentContext.TestDirectory,
+                    resourceUri.Substring(resourceUri.LastIndexOf(".", resourceUri.LastIndexOf(".") - 1) + 1)));
+            if (cookiesHelper.IsSet)
+            {
+                foreach (Cookie cookie in cookiesHelper.Cookies)
+                {
+                    driver.Manage().Cookies.AddCookie(cookie);
+                }
+                driver.Navigate().Refresh();
+            }
+            else
+            { // authenticate
+                driver.FindElement(By.XPath(@"//a[contains(concat(' ',@href,' '),' /sign_in ')]")).Click();
+                
+                driver.FindElement(By.XPath(@"//input[contains(concat(' ',@id,' '),' user_email ')]")).SendKeys(@"i1go1by@gmail.com");
+                driver.FindElement(By.XPath(@"//input[contains(concat(' ',@id,' '),' user_password ')]")).SendKeys(@"321654");
+                driver.FindElement(By.XPath(@"//*[contains(concat(' ',@id,' '),' new_user ')]//input[contains(concat(' ',@class,' '),' login-button ')]")).Click();
+
+                new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(drv => driver.FindElements(By.XPath(@"//*[contains(concat(' ',@id,' '),' navbar ')]//img[contains(concat(' ',@alt,' '),' i1go1by@gmail.com ')]")));
+
+                cookiesHelper.Cookies = driver.Manage().Cookies.AllCookies; 
+            }
+            CollectionAssert.IsNotEmpty(driver.FindElements(By.XPath(@"//*[contains(concat(' ',@id,' '),' navbar ')]//img[contains(concat(' ',@alt,' '),' i1go1by@gmail.com ')]")));
         }
+
         [OneTimeTearDown]
         public void CloseDriver()
         {
