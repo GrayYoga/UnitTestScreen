ab2316865cbd3af3248b6ad59b0a849171c65bad
Sergei.Boiko <sergei.boiko@simbirsoft.com>
2017-11-20 15:49:55 +0400

    #104947 -- Задача X8. Selenium Tier 2. Drag n Drop

---
+++ UnitTestScreen/Pages/way2automation.com_way2auto_jquery_droppable.php/W2aDroppablePage.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+
+namespace UnitTestScreen
+{
+    public class W2aDroppablePage : BasePage, IGoToAuthenticate
+    {
+        private By DroppableFrame = By.CssSelector("iframe[src = 'droppable/default.html']");
+        private By Draggable = By.Id("draggable");
+        private By Droppable = By.Id("droppable");
+
+        public W2aDroppablePage(RemoteWebDriver driver) : base(driver)
+        {
+        }
+
+        public W2aDroppablePage() : base()
+        {
+        }
+
+        public W2aDroppablePage SwitchToDroppableFrame()
+        {
+            Driver.SwitchTo().Frame(Driver.FindElement(DroppableFrame));
+            return this;
+        }
+
+        public W2aDroppablePage DragAndDrop()
+        {
+            var actions = new Actions(Driver);
+            actions
+                .DragAndDrop(Driver.FindElement(Draggable), Driver.FindElement(Droppable))
+                .Build()
+                .Perform();
+
+            return this;
+        }
+
+        public string GetDroppableText()
+        {
+            return (Driver.FindElement(Droppable).Text);
+        }
+
+        public IBaseLoginPage GoToLoginPage()
+        {
+            return new W2aDroppablePage(Driver).GoToLoginPage();
+        }
+    }
+}
---
+++ UnitTestScreen/Tests/DroppableTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Drawing;
+using UnitTestScreen.Models;
+
+namespace UnitTestScreen
+{
+    [TestFixture]
+    class DroppableTests
+    {
+        private RemoteWebDriver Driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            Driver = new RemoteWebDriver(new ChromeOptions().ToCapabilities());
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Driver.Manage().Window.Size = new Size(1360, 1020);
+
+            var config = TestsConfig.GetConfig(GetType().ToString());
+
+            Driver.Navigate().GoToUrl(config.ResourceUri);
+            AuthenticateHelper.Authenticate<W2aMenuPage>(Driver, config);
+            // после авторизации переход происходит не на ту страницу, с которой начиналась авторизация
+            Driver.Navigate().Refresh();
+            Driver.Navigate().GoToUrl(config.ResourceUri);
+        }
+
+        [Test]
+        public void DragAndDropTest()
+        {
+            var w2aDroppablePage = new W2aDroppablePage(Driver);
+
+            w2aDroppablePage.SwitchToDroppableFrame();
+            
+           var DroppableTextBefore = w2aDroppablePage.GetDroppableText();
+
+            w2aDroppablePage.DragAndDrop();
+
+            var DroppableTextAfter = w2aDroppablePage.GetDroppableText();
+
+            Assert.That(!DroppableTextAfter.Equals(DroppableTextBefore));
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            Driver.Quit();
+        }
+    }
+}
