34b646a0e3a874bb1d9184bc0716ef268073d9e4
Sergei.Boiko <sergei.boiko@simbirsoft.com>
2017-11-20 14:05:17 +0400

    Clean code

--- UnitTestScreen/Tests/MenuTests.cs
+++ UnitTestScreen/Tests/MenuTests.cs
@@ -2,16 +2,12 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnitTestScreen.Models;
 
 namespace UnitTestScreen.Tests
 {
-    
+
     [TestFixture]
     public class MenuTests
     {
